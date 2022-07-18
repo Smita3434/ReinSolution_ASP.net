36969129a7431dc1a15bfadc9f6d147e09332b0a
Smita3434 <sayalig94@gmail.com>
2022-07-18 14:14:52 +0530

    Add project files.

---
+++ ReinSolution_ASP.net/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReinSolution_ASP.net.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult PersonalDetails()
+        {
+            List<string> option = new List<string>();
+            option.Add("Select an option");
+            option.Add("Yes");
+            option.Add("No");
+            ViewData["option"] = new SelectList(option);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PersonalDetails(IFormCollection form, ICollection<string> hobbies)
+        {
+            ViewBag.Name = form["username"];
+            ViewBag.Gender = form["Gender"];
+            ViewBag.Hobbies = hobbies;
+            ViewBag.Option = form["option"];
+            return View("Display");
+        }
+
+    }
+}
---
+++ ReinSolution_ASP.net/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReinSolution_ASP.net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReinSolution_ASP.net.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            List<Product> plist = new List<Product>()
+            {
+                new Product{Id=101,Name="HP Laptop",Price=55000},
+                new Product{Id=102,Name="Dell Laptop",Price=75000},
+                new Product{Id=103,Name="Mac Laptop",Price=87000},
+            };
+
+            ViewBag.Productlist = plist;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(IFormCollection form)
+        {
+            ViewBag.Id = form["pid"];
+            ViewBag.Name = form["pname"];
+            ViewBag.Price = form["price"];
+            return View("Details");
+        }
+    }
+}
